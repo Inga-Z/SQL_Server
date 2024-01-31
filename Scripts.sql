24481cf6b34780d5e2fb1ea8810b4086abc68a08
Inga <55615906+Inga-Z@users.noreply.github.com>
2024-01-31 17:08:23 +0300

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -1,15 +1,3 @@
--- all information about backup
-SELECT bs.database_name, bm.physical_device_name, 
-CASE bs.[type]
-WHEN 'D' THEN 'Full'
-WHEN 'I' THEN 'Diff'
-WHEN 'L' THEN 'Log'
-END 'type', bs.backup_size/1024/1024/1024 'size',
-bs.backup_start_date, bs.backup_finish_date, bs.[user_name]
-FROM msdb..backupset bs
-JOIN msdb..backupmediafamily bm ON bs.media_set_id = bm.media_set_id
-ORDER BY backup_start_date DESC
-
 -- Query to List All Jobs with Owners
 SELECT s.name AS JobName, l.name AS JobOwner
 FROM msdb..sysjobs s
@@ -21,32 +9,6 @@ ORDER by l.name
 SELECT NAME AS JobName, SUSER_SNAME(owner_sid) AS JobOwner
 FROM msdb..sysjobs
 ORDER BY NAME
-
--- backup history
-select
-bs.database_name
-,[backup_type] = case bs.type when 'D' then 'full' when 'I' then 'differential' when 'L' then 'log' end
-,bs.name
-,bs.user_name
-,bs.backup_start_date
-,bs.backup_finish_date
-,bmf.physical_device_name
-,bs.is_damaged
-,bs.is_copy_only
-,bs.server_name
-,bs.machine_name
-,cast(round(bs.backup_size/1048576,2) as decimal(10,0)) as 'backup_size_mb'
-,cast(round(bs.compressed_backup_size/1048576,2) as decimal(10,0)) as 'compressed_backup_size_mb'
-from msdb.dbo.backupset bs
-left join msdb.dbo.backupmediafamily bmf on bmf.media_set_id=bs.media_set_id
-where
-bs.backup_finish_date > dateadd(dd,-1,getdate())
--- and bs.database_name in ('PADM')
--- and bs.database_name like 'Flex%'
-and bs.type = 'L' -- log backup
-or bs.type = 'D' -- full backup
--- and bs.type = 'I' -- diff backup
-order by bs.backup_finish_date desc
 
 -- файлы БД с размером в Mb 
 SELECT  @@SERVERNAME AS Server ,
