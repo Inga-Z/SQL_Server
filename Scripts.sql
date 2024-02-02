dc3d6db29dacf4762692c17796be586cb51e844d
Inga <55615906+Inga-Z@users.noreply.github.com>
2024-02-02 19:04:12 +0300

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -111,3 +111,16 @@ SELECT ss.server_id
         ON ss.server_id = sl.server_id 
  LEFT JOIN sys.server_principals ssp 
         ON ssp.principal_id = sl.local_principal_id
+
+SELECT  
+    c.session_id, c.net_transport, c.encrypt_option,  
+    c.auth_scheme, s.host_name, s.program_name,  
+    s.client_interface_name, s.login_name, s.nt_domain,  
+    s.nt_user_name, s.original_login_name, c.connect_time,  
+    s.login_time,
+    t.text
+FROM sys.dm_exec_connections AS c
+JOIN sys.dm_exec_sessions AS s
+    ON c.session_id = s.session_id
+CROSS APPLY sys.dm_exec_sql_text(c.most_recent_sql_handle) AS t
+WHERE s.host_name like N'%MI0673P%'
