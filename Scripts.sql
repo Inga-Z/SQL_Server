4103d4ba68d80368602d2a828740c3a69de90fbf
Inga <55615906+Inga-Z@users.noreply.github.com>
2024-02-02 19:03:08 +0300

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -81,3 +81,33 @@ from sysjobsteps
     left join dbo.sysproxies on sysjobsteps.proxy_id = sysproxies.proxy_id
     left join dbo.sysjobs on sysjobsteps.job_id = sysjobs.job_id
 where sysjobsteps.proxy_id > 0
+
+--линкованный сервер
+SELECT ss.server_id 
+          ,ss.name 
+          ,'Server ' = Case ss.Server_id 
+                            when 0 then 'Current Server' 
+                            else 'Remote Server' 
+                            end 
+          ,ss.product 
+          ,ss.provider 
+          ,ss.catalog 
+          ,'Local Login ' = case sl.uses_self_credential 
+                            when 1 then 'Uses Self Credentials' 
+                            else ssp.name 
+                            end 
+           ,'Remote Login Name' = sl.remote_name 
+           ,'RPC Out Enabled'    = case ss.is_rpc_out_enabled 
+                                   when 1 then 'True' 
+                                   else 'False' 
+                                   end 
+           ,'Data Access Enabled' = case ss.is_data_access_enabled 
+                                    when 1 then 'True' 
+                                    else 'False' 
+                                    end 
+           ,ss.modify_date 
+      FROM sys.Servers ss 
+ LEFT JOIN sys.linked_logins sl 
+        ON ss.server_id = sl.server_id 
+ LEFT JOIN sys.server_principals ssp 
+        ON ssp.principal_id = sl.local_principal_id
