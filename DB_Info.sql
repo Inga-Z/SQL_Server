25d677aa697f1f8808f20281d1a9cf3efe4b9ef5
Inga <55615906+Inga-Z@users.noreply.github.com>
2024-02-16 12:45:21 +0300

    Update DB_Info.sql

--- DB_Info.sql
+++ DB_Info.sql
@@ -6,4 +6,17 @@ SELECT
     , total_size_mb = CAST(SUM(size) * 8. / 1024 AS DECIMAL(8,2))
 FROM sys.master_files WITH(NOWAIT)
 WHERE database_id > 4 --database_id = DB_ID('db_name') -- for db 
-GROUP BY database_idid
+GROUP BY database_id
+
+--проверить % выполнение команд и сколько времени до завершения
+select	r.session_id as SPID, 
+		command, 
+		a.text as query, 
+		start_time, 
+		percent_complete,
+		dateadd(second, estimated_completion_time/1000, GETDATE()) as estimated_completion_time
+from sys.dm_exec_requests r
+cross apply sys.dm_exec_sql_text(r.sql_handle) a
+where r.command in ('BACKUP DATABASE', 'RESTORE DATABASE', 'BACKUP LOG', 'RESTORE LOG',
+					'ALTER INDEX', 'KILLED/ROLLBACK')
+					or r.command like '%DBCC%' or r.command like 'RESTORE%'
