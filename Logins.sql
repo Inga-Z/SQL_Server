31daa194f8f6f17618a15080dcbd7717736ebb45
Inga <55615906+Inga-Z@users.noreply.github.com>
2024-03-06 13:16:25 +0300

    Update Logins.sql

--- Logins.sql
+++ Logins.sql
@@ -1,3 +1,11 @@
+--обнаружить утративших связь с учетной записью пользователей в SQL Server 
+SELECT dp.type_desc, dp.sid, dp.name AS user_name  
+FROM sys.database_principals AS dp  
+LEFT JOIN sys.server_principals AS sp  
+    ON dp.sid = sp.sid  
+WHERE sp.sid IS NULL  
+    AND dp.authentication_type_desc = 'INSTANCE';
+
 --list of logins and last time each logged in
 SELECT [Login] = login_name 
 	,[Last Login Time] = MAX(login_time)
